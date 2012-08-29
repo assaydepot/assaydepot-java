491f9ef5d87f2046804c919c28d1493c67ca8aae
Robert Beltran <robert@jphat.com>
2012-08-29 09:55:58 -0700

    cleaned up the naming

--- src/test/java/com/assaydepot/AssayDepotTest.java
+++ src/test/java/com/assaydepot/AssayDepotTest.java
@@ -12,8 +12,8 @@ public class AssayDepotTest extends TestCase {
 	public void testProviderQuery() throws Exception {
 		Configuration conf = new Configuration();
 		conf.setApiToken("5ae0a040967efe332d237277afb6beca");
-		AssayDepot assDeep = AssayDepotFactory.getAssayDepot( conf );
-		Results  results = assDeep.getProviderRefs( "antibody" );
+		AssayDepot assayDepot = AssayDepotFactory.getAssayDepot( conf );
+		Results  results = assayDepot.getProviderRefs( "antibody" );
 		assertEquals( results.getFacets().size() > 0, true );
 		assertEquals( results.getProviderRefs().size() > 0, true );
 	}
@@ -21,8 +21,8 @@ public class AssayDepotTest extends TestCase {
 	public void testGetProvider() throws Exception {
 		Configuration conf = new Configuration();
 		conf.setApiToken("5ae0a040967efe332d237277afb6beca");
-		AssayDepot assDeep = AssayDepotFactory.getAssayDepot( conf );
-		Provider  provider = assDeep.getProvider( "5dad9ca114072e30801bc31de19fae1d" );
+		AssayDepot assayDepot = AssayDepotFactory.getAssayDepot( conf );
+		Provider  provider = assayDepot.getProvider( "5dad9ca114072e30801bc31de19fae1d" );
 		assertEquals( provider.getId(), "5dad9ca114072e30801bc31de19fae1d" );
 		assertEquals( provider.getKeywords().size() > 0, true );
 		assertEquals( provider.getCertifications().size() > 0, true );
@@ -32,8 +32,8 @@ public class AssayDepotTest extends TestCase {
 	public void testGetWareRefs() throws Exception {
 		Configuration conf = new Configuration();
 		conf.setApiToken("5ae0a040967efe332d237277afb6beca");
-		AssayDepot assDeep = AssayDepotFactory.getAssayDepot( conf );
-		Results  results = assDeep.getWareRefs( "antibody" );
+		AssayDepot assayDepot = AssayDepotFactory.getAssayDepot( conf );
+		Results  results = assayDepot.getWareRefs( "antibody" );
 		assertEquals( results.getWareRefs().size() > 0, true );
 		assertEquals( results.getFacets().size() > 0, true );
 		assertEquals( results.getWareRefs().get(0).getUrls().size() > 0, true );
@@ -42,8 +42,8 @@ public class AssayDepotTest extends TestCase {
 	public void testGetWare() throws Exception {
 		Configuration conf = new Configuration();
 		conf.setApiToken("5ae0a040967efe332d237277afb6beca");
-		AssayDepot assDeep = AssayDepotFactory.getAssayDepot( conf );
-		Ware  ware = assDeep.getWare( "00ca892864cb56087711f438764ea31e" );
+		AssayDepot assayDepot = AssayDepotFactory.getAssayDepot( conf );
+		Ware  ware = assayDepot.getWare( "00ca892864cb56087711f438764ea31e" );
 		assertEquals( ware.getId(), "00ca892864cb56087711f438764ea31e" );
 		assertEquals( ware.getSnippet().contains( "Take your antibody-based therapeutic"), true );
 		assertEquals( ware.getTurnAroundTime().size() > 2, true );
